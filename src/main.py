680ee41ebed41287e1b5993528dd970e877256e6
Eugene Vorontsov <sejeenn@gmail.com>
2024-10-04 20:12:23 +0300

    Add main.py test_sum.py test_sum.yaml

---
+++ src/main.py
@@ -0,0 +1,3 @@
+# Простейшая функция сложения двух целых чисел
+def sum_two_int_nums(a: int, b: int) -> int:
+    return a + b
---
+++ tests/test_sum.py
@@ -0,0 +1,6 @@
+from src.main import sum_two_int_nums
+
+
+def test_sum_two_int_nums():
+    result = sum_two_int_nums(2, 2)
+    assert result == 4
